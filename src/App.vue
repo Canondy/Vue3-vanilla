c3973aa49d58c12b71bc64438228c2a7dcfa1d07
liuhuan <1589328059@qq.com>
2023-09-08 21:41:04 +0800

    vue3项目初始化

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="center-in-center">Hello World！</div>
+</template>
+
+<style scoped>
+.center-in-center{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+}
+</style>
